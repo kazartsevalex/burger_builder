3328305aed1ae4864c90b0a05f88c88c2b18e690
Alex K <damned182@mail.ru>
2019-08-21 20:48:50 +0300

    dont rerender ordersummary when hidden

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,29 +2,35 @@ import React from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => {
-      return (
-        <li key={igKey}>
-          <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
-        </li>
-      );
-    });
+class OrderSummary extends React.Component {
+  componentWillUpdate() {
+    console.log('order summary will update')
+  }
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total sum: {props.totalPrice.toFixed(2)}</strong></p>
-      <p>Continue checkout?</p>
-      <Button buttonType="Danger" clicked={props.cancelPurchase}>Cancel</Button>
-      <Button buttonType="Success" clicked={props.continuePurchase}>CONTINUE</Button>
-    </Aux>
-  );
-};
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients)
+      .map(igKey => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
+          </li>
+        );
+      });
 
-export default orderSummary;
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total sum: {this.props.totalPrice.toFixed(2)}</strong></p>
+        <p>Continue checkout?</p>
+        <Button buttonType="Danger" clicked={this.props.cancelPurchase}>Cancel</Button>
+        <Button buttonType="Success" clicked={this.props.continuePurchase}>CONTINUE</Button>
+      </Aux>
+    );
+  };
+}
+
+export default OrderSummary;
