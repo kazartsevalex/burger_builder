5b5215c0a4ea1dab59eac9430b32ee8e9e0058f6
Alex K <damned182@mail.ru>
2019-09-20 23:04:10 +0300

    move checkValidity to utility.js

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,7 +8,7 @@ import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
-import { updateObject } from '../../../shared/utility';
+import { updateObject, checkValidity } from '../../../shared/utility';
 
 class ContactData extends React.Component {
   state = {
@@ -96,40 +96,11 @@ class ContactData extends React.Component {
     formIsValid: false
   }
 
-  checkValidity(value, rules) {
-    let isValid = true;
-    if (!rules) return true;
-
-    if (rules.required) {
-      isValid = value.trim() !== '' && isValid;
-    }
-
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid;
-    }
-
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid;
-    }
-
-    if (rules.isEmail) {
-      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-      isValid = pattern.test(value) && isValid;
-    }
-
-    if (rules.isNumeric) {
-      const pattern = /^\d+$/;
-      isValid = pattern.test(value) && isValid;
-    }
-
-    return isValid;
-  }
-
   inputChangedHandler = (e, inputIdentifier) => {
     const updFormElement = updateObject(this.state.orderForm[inputIdentifier], {
       value: e.target.value,
       touched: true,
-      valid: this.checkValidity(e.target.value, this.state.orderForm[inputIdentifier].validation)
+      valid: checkValidity(e.target.value, this.state.orderForm[inputIdentifier].validation)
     });
 
     const updatedOrderForm = updateObject(this.state.orderForm, {
